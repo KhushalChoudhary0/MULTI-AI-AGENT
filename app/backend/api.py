02f509b05d53f7220057506faca9a72fe356cac3
KhushalChoudhary0 <khushalboy15@gmail.com>
2026-05-15 18:49:13 +0530

    First commit

---
+++ app/backend/api.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from typing import List
+from app.core.ai_agent import get_response_from_ai_agents
+from app.config.settings import settings
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+import os
+import traceback
+
+app = FastAPI(title="MULTI AI AGENT")
+
+class RequestState(BaseModel):
+    model_name:str
+    system_prompt:str
+    messages:List[str]
+    allow_search: bool
+
+@app.post("/chat")
+def chat_endpoint(request:RequestState):
+    logger.info(f"Received request for model : {request.model_name}")
+
+    if request.model_name not in settings.ALLOWED_MODEL_NAMES:
+        logger.warning("Invalid model name")
+        raise HTTPException(status_code=400 , detail="Invalid model name")
+    
+    try:
+        response = get_response_from_ai_agents(
+            request.model_name,
+            request.messages,
+            request.allow_search,
+            request.system_prompt
+        )
+
+        logger.info(f"Sucesfully got response from AI Agent {request.model_name}")
+
+        return {"response" : response}
+    
+    except Exception as e:
+        logger.error(f"Error occurred during response generation: {str(e)}")
+        traceback.print_exc()
+        
+        # Check if keys are missing
+        groq_key = os.getenv("GROQ_API_KEY")
+        tavily_key = os.getenv("TAVILY_API_KEY")
+        if not groq_key or not tavily_key:
+            error_detail = "API keys are missing. Ensure .env file is in the project root."
+        else:
+            error_detail = str(e)
+
+        raise HTTPException(
+            status_code=500 , 
+            detail=error_detail
+            )
+    
+
+
+
